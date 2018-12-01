d06971bde0b2c15040963a8265bbde1bda1293af
iBlitzkriegi <iBlitzkriegi@users.noreply.github.com>
2018-11-30 22:23:37 -0500

    First push

---
+++ README.md
@@ -0,0 +1,8 @@
+# Dex Permissions
+My current WIP permissions plugin
+
+# Planned Features
+
+- Add the ability to even parse permissions
+- Add group permissions
+- Allow for per member prefixes
---
+++ src/main/java/me/iblitzkriegi/dexpermissions/DexPermissions.java
@@ -0,0 +1,17 @@
+package me.iblitzkriegi.dexpermissions;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DexPermissions extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        toString();
+        toString();
+    }
+}
