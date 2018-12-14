cb6740f26bc63a3c367def3e0f3d0846d6d5475d
iBlitzkriegi <iBlitzkriegi@users.noreply.github.com>
2018-12-14 17:59:56 -0500

    Add getUniqueId util method
    This just toStrings the uuid to avoid repeated usage

--- src/main/java/me/iblitzkriegi/dexpermissions/util/Util.java
+++ src/main/java/me/iblitzkriegi/dexpermissions/util/Util.java
@@ -1,5 +1,11 @@
 package me.iblitzkriegi.dexpermissions.util;
 
+import org.bukkit.entity.Player;
+
 public class Util {
 
+    public static String getUniqueId(Player player) {
+        return player.getUniqueId().toString();
+    }
+    
 }
