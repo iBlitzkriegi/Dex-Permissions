cf49f886fe29d14e9ecfc89c8942c270831479cf
iBlitzkriegi <iBlitzkriegi@users.noreply.github.com>
2018-12-16 20:55:45 -0500

    Add ability to add permissions
    This also now parses player specific permissions in setupPermissions.

--- src/main/java/me/iblitzkriegi/dexpermissions/util/managers/PermissionManager.java
+++ src/main/java/me/iblitzkriegi/dexpermissions/util/managers/PermissionManager.java
@@ -6,7 +6,9 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.permissions.PermissionAttachment;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class PermissionManager {
 
@@ -21,11 +23,33 @@ public class PermissionManager {
         for (String user : config.getConfigurationSection("Users").getKeys(false)) {
             if (uuid.equalsIgnoreCase(user)) {
                 String group = config.getConfigurationSection("Users." + uuid).getString("group");
-                System.out.println(group);
                 for (String permission : config.getConfigurationSection("Groups." + group).getStringList("permissions")) {
                     permissionAttachment.setPermission(permission, true);
                 }
+                for (String permission : config.getStringList("Users." + uuid + ".permissions")) {
+                    permissionAttachment.setPermission(permission, true);
+                }
             }
+        }
+    }
+
+    public static void addPermission(Player player, String permission) {
+        String uuid = Util.getUniqueId(player);
+        PermissionAttachment permissionAttachment = playerPermissions.get(uuid);
+        FileConfiguration config = ConfigManager.getInstance().config;
+        permissionAttachment.setPermission(permission, true);
+        String path = "Users." + uuid + ".permissions";
+        List<String> permissions = config.getStringList(path);
+        if (permission.isEmpty()) {
+            List<String> setPermissions = new ArrayList<>();
+            setPermissions.add(permission);
+            config.set(path, setPermissions);
+        } else {
+            if (permissions.contains(permission)) {
+                return;
+            }
+            permissions.add(permission);
+            config.set(path, permissions);
         }
     }
 
