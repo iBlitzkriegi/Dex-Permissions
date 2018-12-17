dd79ebea40166db1fdc10e375e4a253deb73073e
iBlitzkriegi <iBlitzkriegi@users.noreply.github.com>
2018-12-16 21:45:45 -0500

    Add util method to send message
    Need to add chat formatting at the end of the foundation phase

--- src/main/java/me/iblitzkriegi/dexpermissions/util/Util.java
+++ src/main/java/me/iblitzkriegi/dexpermissions/util/Util.java
@@ -1,11 +1,17 @@
 package me.iblitzkriegi.dexpermissions.util;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class Util {
 
     public static String getUniqueId(Player player) {
         return player.getUniqueId().toString();
+    }
+
+    public static void sendMessage(CommandSender commandSender, String message) {
+        commandSender.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
     }
 
 }
