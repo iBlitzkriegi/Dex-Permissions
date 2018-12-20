116a68b0e8ed439b9300ec32062450cc16d18967
iBlitzkriegi <iBlitzkriegi@users.noreply.github.com>
2018-12-19 21:19:39 -0500

    Add group deleting and rework help messages
    Now help command split into categorical arguments, either group help or user help

--- src/main/java/me/iblitzkriegi/dexpermissions/DexCommand.java
+++ src/main/java/me/iblitzkriegi/dexpermissions/DexCommand.java
@@ -4,6 +4,7 @@ import me.iblitzkriegi.dexpermissions.util.Util;
 import me.iblitzkriegi.dexpermissions.util.managers.ConfigManager;
 import me.iblitzkriegi.dexpermissions.util.managers.PermissionManager;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -19,7 +20,14 @@ public class DexCommand implements CommandExecutor {
             return true;
         }
         if (args.length == 0) {
-            sendHelpMessage(commandSender);
+            sendInvalidUsage(commandSender);
+            return true;
+        } else if (args.length == 1) {
+            String category = args[0].toLowerCase();
+            if (!(category.contains("group") || category.contains("user"))) {
+                sendInvalidUsage(commandSender);
+            }
+            sendHelpMessage(commandSender, category);
             return true;
         }
         String argument = args[0].toLowerCase();
@@ -30,13 +38,13 @@ public class DexCommand implements CommandExecutor {
                 break;
             case "group":
                 if (args.length <= 1) {
-                    sendHelpMessage(commandSender);
+                    sendInvalidUsage(commandSender);
                     return true;
                 }
                 String firstArgument = args[1];
                 if (firstArgument.equalsIgnoreCase("set")) {
                     if (args.length != 4) {
-                        sendHelpMessage(commandSender);
+                        sendInvalidUsage(commandSender);
                         return true;
                     }
                     Player player = Bukkit.getServer().getPlayer(args[2]);
@@ -48,11 +56,17 @@ public class DexCommand implements CommandExecutor {
                     return true;
                 } else if (firstArgument.equalsIgnoreCase("create")) {
                     if (args.length != 3) {
-                        sendHelpMessage(commandSender);
+                        sendInvalidUsage(commandSender);
                         return true;
                     }
                     PermissionManager.createGroup(args[2]);
                     return true;
+                } else if (firstArgument.equalsIgnoreCase("delete")) {
+                    if (args.length != 3) {
+                        sendInvalidUsage(commandSender);
+                        return true;
+                    }
+                    PermissionManager.deleteGroup(args[2]);
                 }
                 ConfigurationSection groupSection = ConfigManager.getInstance().getConfig().getConfigurationSection("Groups." + args[1]);
                 if (groupSection == null) return true;
@@ -75,7 +89,7 @@ public class DexCommand implements CommandExecutor {
                         return true;
                     }
                     if (!args[2].equalsIgnoreCase("permission")) {
-                        sendHelpMessage(commandSender);
+                        sendInvalidUsage(commandSender);
                         return true;
                     }
                     if (args[3].equalsIgnoreCase("add")) {
@@ -89,19 +103,38 @@ public class DexCommand implements CommandExecutor {
 
 
             default:
-                sendHelpMessage(commandSender);
+                sendInvalidUsage(commandSender);
         }
 
         return true;
     }
 
-    private void sendHelpMessage(CommandSender player) {
-        //TODO this
+    private void sendHelpMessage(CommandSender commandSender, String type) {
+        StringBuilder helpMessage = new StringBuilder();
+        helpMessage.append("&b&l------- &fDex-Permissions &b&l-------------------\n");
+        if (type.contains("group")) {
+            helpMessage.append("&8Use /dex group <argument> to use each listed item\n");
+            helpMessage.append("&bset <user> <group> - &fSet a users group\n");
+            helpMessage.append("&bcreate <name> - &fCreate a new group\n");
+            helpMessage.append("&bdelete <name> - &fDelete a group\n");
+            helpMessage.append("&b<group> <permissions> - &fGet a groups perms\n");
+            helpMessage.append("&b<group> set prefix <prefix> - &fSet chat prefix\n");
+            helpMessage.append("&b<group> set suffix <suffix> - &fSet chat suffix\n");
+            helpMessage.append("&b<group> permission add <perm> - &fAdd perms\n");
+            helpMessage.append("&b<group> permission remove <perm> - &fRemove perms\n");
+        } else if (type.contains("user")) {
+            
+        }
+        commandSender.sendMessage(ChatColor.translateAlternateColorCodes('&', helpMessage.toString()));
     }
 
-    public String getArgs(String[] args, int num){
+    private void sendInvalidUsage(CommandSender commandSender) {
+        Util.sendMessage(commandSender, "Incorrect usage! Use /dex <group|user>");
+    }
+
+    public String getArgs(String[] args, int num) {
         StringBuilder sb = new StringBuilder();
-        for(int i = num; i < args.length; i++) {
+        for (int i = num; i < args.length; i++) {
             sb.append(args[i]).append(" ");
         }
         return sb.toString().trim();
